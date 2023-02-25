a56a99857dffd1ce4f97c614c0fe516310a1f086
shruti-222 <shrutiambekar222@gmail.com>
2023-02-25 21:53:01 +0530

    add requirements.txt
    Signed-off-by: shruti-222 <shrutiambekar222@gmail.com>

--- app.py
+++ app.py
@@ -27,6 +27,6 @@ if plot:
     else:
         #plot for state
         state_df =df[df['State']== selected_state]
-        fig = px.scatter_mapbox(state_df, lat="Latitude", lon="Longitude", size=primary, color=secondary, zoom=5, size_max=35,
+        fig = px.scatter_mapbox(state_df, lat="Latitude", lon="Longitude", size=primary, color=secondary, zoom=6, size_max=35,
                                 mapbox_style="carto-positron", width=1200, height=700,hover_name='District')
         st.plotly_chart(fig, use_container_width=True)
